96bc3c50a21abcee4f172f49e0486921480d628f
MeyfroidtArthur <arthur.meyfroidt@student.howest.be>
2026-01-28 00:16:03 +0100

    Update build script

--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -447,7 +447,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/2.1/content/47dfcee7-b142-4a17-b126-1fdfa6124304/github/main",
         queries,
       })
     )
